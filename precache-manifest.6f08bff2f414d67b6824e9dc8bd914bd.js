25ea257e664ad3dcd55adb28f1aafa920c282695
Vishnu Chaitanya Vutla <vichaita@microsoft.com>
2020-01-11 19:39:50 +0530

    Updates

---
+++ precache-manifest.6f08bff2f414d67b6824e9dc8bd914bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cff5ffc23bf87a0757d71a1ea23fdd84",
+    "url": "/monsters-roledex/index.html"
+  },
+  {
+    "revision": "e7dd20a2f5fa165a3f30",
+    "url": "/monsters-roledex/static/css/main.846d9e5a.chunk.css"
+  },
+  {
+    "revision": "a6020b8e85980073f1d0",
+    "url": "/monsters-roledex/static/js/2.7996c63c.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-roledex/static/js/2.7996c63c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e7dd20a2f5fa165a3f30",
+    "url": "/monsters-roledex/static/js/main.b95fe3b0.chunk.js"
+  },
+  {
+    "revision": "38a76ad6daedf634acf3",
+    "url": "/monsters-roledex/static/js/runtime-main.ea6763b8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-roledex/static/media/logo.5d5d9eef.svg"
+  }
+]);
